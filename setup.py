5b0cf81268c102f3d53fadf66596c4f7d80ca2e3
jay ess <mentoce@gmail.com>
2020-05-02 22:14:08 -0700

    Add more metadata to setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from setuptools import setup, find_packages
 
 
-description = ('Contains the text from the memorandum ',
-               'of the July 25, 2019 phone call')
+description = 'Contains the text from the memorandum ',
+              'of the July 25, 2019 phone call'
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
 setup(
     name='The Phone Call',
     version='0.0.1',
     description=description,
+    long_description=readme,
     author='Jay Ess',
-    include_package_data=True
+    include_package_data=True,
+    license='MIT',
+    license_file=license,
+    url='https://github.com/jay3ss/thephonecall'
 )
